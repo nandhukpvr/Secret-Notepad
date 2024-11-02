b2c60562ef131cccae1b0555a181d1fdbe15c2b0
Joyal James <joyaljms98@gmail.com>
2024-11-02 13:11:45 +0530

    Added Button to save

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog
 import random
+import os
 
 def hide_text(event=None):
     mainwindow.config(fg="#f7f5f5")
@@ -7,6 +9,14 @@ def character_count_update(event):
     content=mainwindow.get("1.0", tk.END)
     c=len(content)-1
     char_count_label.config(text=f"Character Count : {c}")
+def save_file(event=None):
+    download_path = os.path.expanduser("~/Downloads")
+    file_name = "secret_note.txt"
+    file_path = os.path.join(download_path, file_name)
+    with open(file_path,'w') as file:
+        content=mainwindow.get("1.0",tk.END)
+        file.write(content)
+        print("File saved")
 
 root = tk.Tk()
 root.title("Secret Notepad with Encryption")
@@ -15,6 +25,10 @@ root.configure(bg="#c4c4c4")
 
 mainwindow = tk.Text(root, width=80, height=15,font=("Verdana" ,20), wrap="word",fg="#000000", bg="#f7f5f5", insertbackground="black")
 mainwindow.pack(pady=10, padx=10)
+
+save_button=tk.Button(root , text="Save the secret message with Encryption",font=("Verdana",12), command=save_file)
+save_button.pack(pady=10)
+
 
 
 def on_text_change(event):
